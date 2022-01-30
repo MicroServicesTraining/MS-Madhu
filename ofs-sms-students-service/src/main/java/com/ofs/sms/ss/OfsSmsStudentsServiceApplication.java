566e5e4da8d8a7000340e97a937d07c2b5749d1e
sekharbabu4patnam <babuthetrainer@gmail.com>
2022-01-30 07:46:44 +0530

    removed unnecessary comments

--- ofs-sms-students-service/src/main/java/com/ofs/sms/ss/OfsSmsStudentsServiceApplication.java
+++ ofs-sms-students-service/src/main/java/com/ofs/sms/ss/OfsSmsStudentsServiceApplication.java
@@ -2,7 +2,7 @@ package com.ofs.sms.ss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// hkjh;hjhlhj;lhl;hk
+
 @SpringBootApplication
 public class OfsSmsStudentsServiceApplication {
 
