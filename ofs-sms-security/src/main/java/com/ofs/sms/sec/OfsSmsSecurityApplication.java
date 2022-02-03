1ab50f1dd83d5d81e3eed37a50ceead310c45d56
sekharbabu4patnam <babuthetrainer@gmail.com>
2022-02-03 06:28:38 +0530

    Spring Security Demo

---
+++ ofs-sms-security/src/main/java/com/ofs/sms/sec/OfsSmsSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ofs.sms.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfsSmsSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfsSmsSecurityApplication.class, args);
+	}
+
+}
